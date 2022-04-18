daf0ae596d5d2b95b8656f48da305e3203bfee33
Saara Leppis <saara@leppihalme.fi>
2022-04-18 21:10:05 +0300

    step3 data in objects

--- osa1/kurssitiedot/README.md
+++ osa1/kurssitiedot/README.md
@@ -1,3 +1,5 @@
 # osa 1
 
 ## kurssitiedot
+
+step 1 & 2: 1 hr
--- osa1/kurssitiedot/src/components/Content.js
+++ osa1/kurssitiedot/src/components/Content.js
@@ -4,9 +4,9 @@ import Part from './Part';
 const Content = props => {
     return (
         <div>
-            <Part part={props.part1} exercise={props.exercise1} />
-            <Part part={props.part2} exercise={props.exercise2} />
-            <Part part={props.part3} exercise={props.exercise3} />
+            <Part part={props.part1} />
+            <Part part={props.part2} />
+            <Part part={props.part3} />
         </div>
     );
 };

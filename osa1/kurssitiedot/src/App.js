e5f802226198a986e2e20ce6426b1d8ac1564523
Saara Leppis <saara@leppihalme.fi>
2022-04-18 20:03:33 +0300

    step1 Content, Header, Total added

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,15 +1,23 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total"
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
-  const exercise1 = 10;
+  const exercises1 = 10;
   const part2 = 'Using props to pass data';
-  const exercise2 = 10;
+  const exercises2 = 10;
   const part3 = 'Using props to pass data';
-  const exercise3 = 10;
+  const exercises3 = 10;
 
   return (
     <div>
-      <p>Hello World!</p>
+      <Header course={course} />
+      <Content part={part1} excersice={exercises1} />
+      <Content part={part2} excersice={exercises2} />
+      <Content part={part3} excersice={exercises3} />
+      <Total sumOfExcercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 }
---
+++ osa1/kurssitiedot/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total = props => {
+    return (
+        <div>
+            <p>{props.sumOfExcercises}</p>
+        </div>
+    );
+};
+
+export default Total;

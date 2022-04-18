324121333e8ac1417d649d9448a8a2e492805ee7
Saara Leppis <saara@leppihalme.fi>
2022-04-18 21:26:25 +0300

    step3 and step4 - one course object

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -3,30 +3,31 @@ import Content from "./components/Content";
 import Total from "./components/Total"
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   };
 
   return (
     <div>
       <Header
         course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3} />
+      <Content parts={course.parts} />
       <Total
-        sumOfExcercises={part1.exercises + part2.exercises + part3.exercises} />
+        parts={course.parts} />
     </div>
   );
 }

6ab44ff46933496fcdd7fd8d85e5fe782bfbf633
Saara Leppis <saara@leppihalme.fi>
2022-05-24 20:10:31 +0300

    2.6 puhelinluettelo step1 simple functionality to listnames

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,27 +1,63 @@
 import { useState } from 'react'
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const ifNotInList = (name) => {
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === name) {
+        return false
+      }
+      else {
+        return true
+      }
+    };
+  }
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+    // check if name in list already
+    if (ifNotInList(newName)) {
+      setPersons([...persons, { name: newName }]);
+      alert(`${newName} was added`);
+    }
+    else {
+      alert(`${newName} is already in list`)
+    }
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
 
   return (
-    <div>
+    <div className='phonebook-wrapper'>
       <h2>Phonebook</h2>
-      <form>
+
+      <form onSubmit={handleAddName}>
         <div>
-          name: <input />
+          name:
+          <input
+            type="text"
+            value={newName}
+            onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <ul className='numbers'>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
   )
-
 }
 
 export default App

2a6890188c9200accabac52cf43788c91fb8f2bf
Saara Leppis <saara@leppihalme.fi>
2022-05-24 22:31:27 +0300

    2.9 step4 serach field added

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,10 +3,14 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const ifNotInList = (name) => {
     for (let i = 0; i < persons.length; i++) {
@@ -34,15 +38,27 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  }
+    setNewName(event.target.value)
+  };
   const handleNumberChange = (event) => {
-    setPhoneNumber(event.target.value);
-  }
+    setPhoneNumber(event.target.value)
+  };
+  const handleFilter = (event) => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
 
   return (
     <div className='phonebook-wrapper'>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:
+        <input
+          name="filter"
+          type="text"
+          value={filter}
+          onChange={handleFilter} />
+      </div>
 
       <form onSubmit={handleAddName}>
         <div className='inputs-wrapper'>
@@ -64,7 +80,9 @@ const App = () => {
 
       <h2>Numbers</h2>
       <ul className='numbers'>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {
+          persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()) || filter === '').map(person => <li key={person.name}>{person.name} {person.number}</li>)
+        }
       </ul>
     </div>
   )

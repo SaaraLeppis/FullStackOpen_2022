56a203a1d779bf574d56328557c960ab780bce84
Saara Leppis <saara@leppihalme.fi>
2022-06-20 17:50:31 +0300

    models cleaned

--- osa4/blogilista/models/blogs.js
+++ osa4/blogilista/models/blogs.js
@@ -1,14 +1,4 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('Connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('Error connecting to MongoDB', error.message)
-  })
 
 const blogSchema = mongoose.Schema({
   title: String,

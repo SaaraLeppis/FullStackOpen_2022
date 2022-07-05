6e47a149ef00efa7208f806ac61f428ccd93dd6b
Saara Leppis <saara@leppihalme.fi>
2022-07-05 16:24:36 +0300

    4.19 POST with token

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,9 +2,21 @@ require('express-async-errors')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/users')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  console.log('auth', authorization)
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
 
   if (blogs) {
     response.json(blogs)
@@ -21,8 +33,15 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  console.log(token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-  const user = await User.findById(body.userId)
+  // const user = await User.findById(body.userId)
 
   const blog = new Blog({
     title: body.title,
--- osa4/blogilista/controllers/login.js
+++ osa4/blogilista/controllers/login.js
@@ -21,11 +21,11 @@ loginRouter.post('/', async (request, response) => {
     username: user.username,
     id: user._id,
   }
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 })
 
   response
     .status(200)
-    .send({ token, username: user.username, naem: user.name })
+    .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter

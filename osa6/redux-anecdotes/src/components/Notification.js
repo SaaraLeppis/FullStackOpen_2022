8d62328ecb6a1cc34bec5577b77b969cf76a09de
Saara Leppis <saara@leppihalme.fi>
2022-08-08 11:04:24 +0300

    6.19 Notification component with connect

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,22 +1,34 @@
-import { useSelector } from "react-redux"
+// import { useSelector } from "react-redux"
+import { connect } from "react-redux"
 
-const Notification = () => {
+
+const Notification = (props) => {
+  console.log('props', props)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
 
-  const notifications = useSelector(state => state.notification)
+  // const notifications = useSelector(state => state.notification)
 
   return (
     <>
-      {notifications &&
+      {props.notification &&
         < div style={style} >
-          {notifications}
+          {props.notification}
         </div >}
     </>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotifications = connect(mapStateToProps)(Notification)
+export default ConnectedNotifications
+
+// export default Notification
